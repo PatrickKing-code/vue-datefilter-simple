7664028237cc8f8955c40dae75b29f7298d5f90d
Patrick <beiershen@163.com>
2020-01-26 22:44:20 +0800

    all is ok, date resolve

---
+++ src/components/DateFilter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ date }}
+    <p>------------------------</p>
+    {{ date | format("yyyy-MM-dd hh:mm:ss") }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dateFilter",
+  data() {
+    return {
+      date: new Date()
+    };
+  }
+};
+</script>
+
+<style></style>
